b2b8fc9328d057652e063e1ae79af56397aef375
royofficial <85897883+royofficial@users.noreply.github.com>
2021-06-22 20:22:43 +0700

    Update list_pass.py

--- password/list_pass.py
+++ password/list_pass.py
@@ -2,7 +2,7 @@
 # Jika Ingin Mengubah pw_tambahan Harap Gunakan Pemisah Menggunakan |
 pw_tambahan="""
 
-bismillah|sayang
+bismillah|sayang|indonesia|cantik
 
 """.strip().split("|")
 ngntd=((())>(()))+((())>(()))
